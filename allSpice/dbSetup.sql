90c43651b42303c07d06591a12a23de37c332c72
JoeCalvi <joecalvi@me.com>
2023-03-24 14:04:44 -0600

    fixed tables

--- allSpice/dbSetup.sql
+++ allSpice/dbSetup.sql
@@ -6,3 +6,49 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+DROP TABLE IF EXISTS accounts;
+
+DELETE FROM
+accounts
+WHERE id = "";
+
+CREATE TABLE IF NOT EXISTS recipes(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  creatorId VARCHAR(255) NOT NULL COMMENT 'Recipe Creator Id',
+  title VARCHAR(80) NOT NULL COMMENT 'Recipe Title',
+  instructions VARCHAR(1000) NOT NULL COMMENT 'Recipe Instructions',
+  img VARCHAR(500) NOT NULL COMMENT 'Recipe Image',
+  category VARCHAR(50) NOT NULL COMMENT 'Recipe Category',
+
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+DROP TABLE recipes;
+
+INSERT INTO recipes
+(title, instructions, img, category, `creatorId`)
+VALUES
+('Delicious Pasta', 'Make the pasta it\'s not that hard.', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.616.493.suffix/1615916524567.jpeg', 'Pasta', '641b636bc53b98bd6be93ce2');
+
+CREATE TABLE IF NOT EXISTS ingredients(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Ingredient Id',
+  recipeId INT NOT NULL COMMENT 'Recipe Id',
+  name VARCHAR(80) NOT NULL COMMENT 'Ingredient Name',
+  quantity VARCHAR(80) NOT NULL COMMENT 'Ingredient Quantity',
+
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+DROP TABLE IF EXISTS ingredients;
+
+CREATE TABLE IF NOT EXISTS favorites(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Favorite Id',
+  accountId VARCHAR(255) NOT NULL COMMENT 'Account Id',
+  recipeId INT NOT NULL COMMENT 'Recipe Id',
+
+  FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+DROP TABLE IF EXISTS favorites;
