ccaba994a889ab4b509cbf5bc5ff1d5dbfc63ad6
JoeCalvi <joecalvi@me.com>
2023-03-25 14:40:28 -0600

    create favorite gtg

---
+++ allSpice/Services/FavoritesService.cs
@@ -0,0 +1,18 @@
+namespace allSpice.Services
+{
+    public class FavoritesService
+    {
+        private readonly FavoritesRepository _repo;
+
+        public FavoritesService(FavoritesRepository repo)
+        {
+            _repo = repo;
+        }
+
+        internal Favorite CreateFavorite(Favorite favoriteData)
+        {
+            Favorite favorite = _repo.CreateFavorite(favoriteData);
+            return favorite;
+        }
+    }
+}
